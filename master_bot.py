22d671ae40a169b8ccac98a88bb94bc6c78fabb4
e_chupikov <e_chupikov@wargaming.net>
2018-09-24 15:38:30 +0300

    infinity_polling

--- master_bot.py
+++ master_bot.py
@@ -1341,5 +1341,18 @@ def edit_report(call):
         bot.send_message(chat_id=user_id, text=texts.SEEMS_FIRED)
 
 
-if __name__ == '__main__':
-    bot.infinity_polling()
+while True:
+
+    try:
+
+        bot.infinity_polling()
+
+    # ConnectionError and ReadTimeout because of possible timeout of the requests library
+
+    # TypeError for moviepy errors
+
+    # maybe there are others, therefore Exception
+
+    except Exception as e:
+        logging.error(e)
+        time.sleep(5)
